3d85517b245ed9b763f24d5fd7a1493c23a251bf
Michael Lim <malexanderlim@gmail.com>
2025-05-08 23:02:42 -0700

    Updating vite config.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,14 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss()
+    tailwindcss(),
+    tsconfigPaths()
   ], 
   resolve: {
     alias: {
